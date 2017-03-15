05a07847f997ad435c9b9dbb2923c445ad196314
Brendan Leber <brendan.leber@isilon.com>
2017-03-15 15:29:50 -0700

    Day 09 - Part 01 - Explosives in Cyberspace (Solution)
    Working solution for part one of day nine.

--- 2016/09-explosives_in_cyberspace/eic.cpp
+++ 2016/09-explosives_in_cyberspace/eic.cpp
@@ -10,7 +10,42 @@ std::string decompress(std::string input);
 
 std::string decompress(std::string input)
 {
-	return input;
+	std::string output;
+
+	while (!input.empty()) {
+		// std::cerr << "string " << input << '\n';
+
+		if (input[0] != '(') {
+			auto end = input.find('(');
+
+			auto segment = input.substr(0, end);
+			// std::cerr << "segment " << segment << '\n';
+
+			output.append(segment);
+			input.erase(0, end);
+		}
+		else {
+			auto end = input.find(')');
+			auto marker = input.substr(1, end - 1);
+			input.erase(0, end + 1);
+
+			// std::cerr << "marker " << marker << '\n';
+
+			auto num_chars = std::stoi(marker);
+			end = marker.find('x');
+			auto num_times = std::stoi(marker.substr(end + 1));
+
+			auto repeat = input.substr(0, num_chars);
+			input.erase(0, num_chars);
+
+			// std::cerr << "chars " << num_chars << " times " << num_times << " repeat " << repeat << '\n';
+
+			for (int i = 0; i < num_times; ++i)
+				output.append(repeat);
+		}
+	}
+
+	return output;
 }
 
 
