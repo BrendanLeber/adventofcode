7a07f42a565eefb728e1d536e9c56162232fc9e7
Brendan Leber <brendan.leber@isilon.com>
2017-03-15 10:20:03 -0700

    Day 07 - Part 02 - Internet Protocol Version 7 (Solution)
    Working solution for day seven, part two.  The code has been updated
    to use one source file for both solutions.  Again, this is quick,
    working code.  I should really come back and make it more betterer
    someday.

--- 2016/07-internet_protocol_version_7/ipv7.cpp
+++ 2016/07-internet_protocol_version_7/ipv7.cpp
@@ -6,6 +6,7 @@
 #include <utility>
 #include <vector>
 
+
 using Strings = std::vector<std::string>;
 
 struct IP_Address
@@ -28,11 +29,49 @@ struct IP_Address
 using IP_Addresses = std::vector<IP_Address>;
 
 
-
+Strings abas_to_babs(const Strings& abas);
+Strings get_abas(const IP_Address& segment);
 bool has_abba(const std::string& segment);
 IP_Addresses parse_strings(const Strings& strings);
 Strings read_input(std::string in_file);
+bool supports_ssl(const IP_Address& addr);
 bool supports_tls(const IP_Address& addr);
+
+
+Strings abas_to_babs(const Strings& abas)
+{
+	Strings babs;
+	std::string bab { "012" };
+
+	for (const auto& aba : abas) {
+		bab[0] = bab[2] = aba[1];
+		bab[1] = aba[0];
+		babs.push_back(bab);
+	}
+
+	return babs;
+}
+
+Strings get_abas(const IP_Address& addr)
+{
+	Strings abas;
+	for (const auto& segment : addr.supernets) {
+		auto end = segment.length() - 2;
+		for (size_t pos = 0; pos < end; ++pos) {
+			if (segment[pos] != segment[pos+2])
+				continue;
+
+			if (segment[pos] != segment[pos+1])
+				abas.push_back(segment.substr(pos, 3));
+		}
+	}
+
+	std::sort(std::begin(abas), std::end(abas));
+	auto it = std::unique(std::begin(abas), std::end(abas));
+	abas.erase(it, std::end(abas));
+
+	return abas;
+}
 
 
 bool has_abba(const std::string& segment)
@@ -113,6 +152,25 @@ Strings read_input(std::string in_file)
 }
 
 
+bool supports_ssl(const IP_Address& addr)
+{
+	auto abas = get_abas(addr);
+	auto babs = abas_to_babs(abas);
+
+	for (const auto& hypernet : addr.hypernets) {
+		// std::cerr << "* " << hypernet << std::endl;
+		for (const auto& bab : babs) {
+			auto pos = hypernet.find(bab);
+			// std::cerr << "** " << bab << ' ' << pos << std::endl;
+			if (pos != std::string::npos)
+				return true;
+		}
+	}
+
+	return false;
+}
+
+
 bool supports_tls(const IP_Address& addr)
 {
 	bool in_super = false;
@@ -147,13 +205,20 @@ int main(int argc, char** argv)
     auto addrs = parse_strings(input);
 
     size_t how_many_support_tls = 0;
+    size_t how_many_support_ssl = 0;
     for (auto& addr : addrs) {
 	    if (supports_tls(addr)) {
 		    ++how_many_support_tls;
 	    }
+
+	    if (supports_ssl(addr)) {
+		    ++how_many_support_ssl;
+	    }
     }
 
-    std::cout << how_many_support_tls << '\n';
-    
+    std::cout
+	    << "TLS: " << how_many_support_tls << '\n'
+	    << "SSL: " << how_many_support_ssl << '\n';
+
     return EXIT_SUCCESS;
 }
