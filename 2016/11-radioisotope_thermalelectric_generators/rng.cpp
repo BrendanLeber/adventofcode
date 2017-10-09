56d4859ea3cd0ac4e888d822997209ba914dd26f
Brendan Leber <brendan.leber@isilon.com>
2017-10-09 09:18:43 -0700

    Sync to server.

--- 2016/11-radioisotope_thermalelectric_generators/rng.cpp
+++ 2016/11-radioisotope_thermalelectric_generators/rng.cpp
@@ -1,19 +1,48 @@
+#include <array>
 #include <iostream>
 #include <string>
 #include <vector>
 
-using Strings = std::vector<std::string>;
+
+constexpr int Num_Floors = 4;
+
+
+void dump_state();
+std::vector<int> parse_input();
+
+
+void dump_state(const std::vector<int>& state)
+{
+	int num_chips = state.size() / 2;
+	for (int floor = Num_Floors - 1; floor >= 0; --floor) {
+		std::cout
+			<< 'F' << (floor + 1) << " \n";
+	}
+}
+
+
+std::vector<int> parse_input()
+{
+	int num_chips;
+	std::cin >> num_chips;
+
+	std::vector<int> state;
+	state.reserve(num_chips * 2);
+	for (int chip = 0; chip < num_chips * 2; ++chip) {
+		int ch;
+		std::cin >> ch;
+		state.emplace_back(ch);
+	}
+
+	return state;
+}
+
 
 int main()
 {
-    Strings input;
-    while (std::cin) {
-        std::string line;
-        std::getline(std::cin, line);
-        if (!line.empty()) {
-            input.push_back(line);
-        }
-    }
+	auto state = parse_input();
+	std::cout << "initial state:\n";
+	dump_state(state);
 
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
