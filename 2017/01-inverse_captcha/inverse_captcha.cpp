40c2751207df5d3fcd0711c415f20066c506588a
Brendan Leber <brendan.leber@isilon.com>
2017-12-01 13:31:49 -0800

    Day 1 - Part 1 - Inverse Captcha
    Working implementation of the `inverse captcha` problem from day one
    of the 2017 Advent of Code.

--- 2017/01-inverse_captcha/inverse_captcha.cpp
+++ 2017/01-inverse_captcha/inverse_captcha.cpp
@@ -1,3 +1,4 @@
+#include <cctype>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -6,9 +7,20 @@ using Digits = std::vector<int>;
 
 int inverse_captcha(Digits const& digits);
 
-int inverse_captcha(Digits const& /*digits*/)
+int inverse_captcha(Digits const& digits)
 {
-    return 0;
+    int sum = 0;
+    for (auto it = std::begin(digits); it != std::end(digits); ++it) {
+        if (*it == *(it + 1)) {
+            sum += *it;
+        }
+    }
+
+    if (digits.front() == digits.back()) {
+        sum += digits.front();
+    }
+
+    return sum;
 }
 
 int main(int /*argc*/, char** /*argv*/)
@@ -16,9 +28,16 @@ int main(int /*argc*/, char** /*argv*/)
     std::string input;
     std::cin >> input;
 
-    std::cerr << "input length: " << input.size() << '\n';
-
     Digits digits;
+    digits.reserve(input.size());
+    for (auto digit : input) {
+        if (std::isdigit(static_cast<unsigned char>(digit)) != 0) {
+            digits.push_back(static_cast<int>(digit) - '0');
+        }
+    }
+
+    std::cerr << "input length: " << digits.size() << '\n';
+
     std::cout << inverse_captcha(digits) << '\n';
 
     return 0;
