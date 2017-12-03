36a8ee2ee42401e49f99243a217b5dd1ce005abf
Brendan Leber <brendan@brendanleber.com>
2017-12-02 16:23:21 -0800

    Day Two - Part Two - Corruption Checksum
    Updated the solution to solve the second half of the problem.

--- 2017/02-corruption_checksum/corruption_checksum.cpp
+++ 2017/02-corruption_checksum/corruption_checksum.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <iostream>
 #include <limits>
 #include <sstream>
@@ -13,6 +14,28 @@ int corruption_checksum(Sheet const& sheet)
 {
     int checksum = 0;
 
+#if defined(PART_TWO)
+    for (auto const& row : sheet) {
+        bool found = false;
+        int lo = 0, hi = 0;
+        for (size_t i = 0; !found && i < row.size(); ++i) {
+            for (size_t j = i + 1; !found && j < row.size(); ++j) {
+                lo = std::min(row[i], row[j]);
+                hi = std::max(row[i], row[j]);
+                auto d = std::div(hi, lo);
+                if (d.rem == 0) {
+                    found = true;
+                }
+            }
+        }
+
+        if (found) {
+            checksum += hi / lo;
+        } else {
+            std::cerr << "match not found!\n";
+        }
+    }
+#else
     for (auto const& row : sheet) {
         int low = std::numeric_limits<int>::max();
         int high = std::numeric_limits<int>::min();
@@ -24,6 +47,7 @@ int corruption_checksum(Sheet const& sheet)
 
         checksum += high - low;
     }
+#endif
 
     return checksum;
 }
