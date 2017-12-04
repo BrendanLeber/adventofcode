9ecf19ae1eba0c84f1a3e12da5450fe634accf52
Brendan Leber <brendan.leber@isilon.com>
2017-12-04 14:48:50 -0800

    Solution for Day Four, Part Two.

--- 2017/04-high-entropy_passphrases/solver.cpp
+++ 2017/04-high-entropy_passphrases/solver.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <set>
 #include <sstream>
@@ -12,18 +13,19 @@ int solver(std::vector<std::string> const& list)
 
     std::set<std::string> words;
     for (auto const& phrase : list) {
-	words.clear();
-	size_t num_words = 0;
-	std::istringstream str(phrase);
-	std::string word;
-	while (str >> word) {
-	    words.insert(word);
-	    ++num_words;
-	}
+        words.clear();
+        size_t num_words = 0;
+        std::istringstream str(phrase);
+        std::string word;
+        while (str >> word) {
+            std::sort(std::begin(word), std::end(word)); // Part Two
+            words.insert(word);
+            ++num_words;
+        }
 
-	if (words.size() == num_words) {
-	    ++num_valid;
-	}
+        if (words.size() == num_words) {
+            ++num_valid;
+        }
     }
 
     return num_valid;
@@ -34,7 +36,7 @@ int main()
     std::vector<std::string> list;
     std::string line;
     while (std::getline(std::cin, line)) {
-	list.emplace_back(line);
+        list.emplace_back(line);
     }
 
     std::cout << solver(list) << '\n';
