8e891d7ce35d6d9593f1910edfe5e40632a8ea98
Brendan Leber <brendan@brendanleber.com>
2018-11-30 22:17:15 -0800

    Advent of Code 2018.  Day One.  Part Two.
    Working solution for part two as well.

--- 2018/01-chronal/chronal.py
+++ 2018/01-chronal/chronal.py
@@ -5,10 +5,22 @@ import fileinput
 
 
 def part_one(changes: list) -> int:
-    frequency: int = 0
-    for freq in changes:
-        frequency += freq
+    frequency = 0
+    for change in changes:
+        frequency += change
     return frequency
+
+
+def part_two(changes: list) -> int:
+    frequency = 0
+    frequencies_found = set()
+    frequencies_found.add(frequency)
+    while True:
+        for change in changes:
+            frequency += change
+            if frequency in frequencies_found:
+                return frequency
+            frequencies_found.add(frequency)
 
 
 if __name__ == "__main__":
@@ -16,3 +28,4 @@ if __name__ == "__main__":
     for line in fileinput.input():
         frequency_changes.append(int(line))
     print(part_one(frequency_changes))
+    print(part_two(frequency_changes))
