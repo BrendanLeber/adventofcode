2b9dec3ca3111394d9571c7e7b6c59f6f43b9e67
Brendan Leber <brendan@brendanleber.com>
2018-12-01 22:59:35 -0800

    More improvements from using standard packages.

--- 2018/02-inventory/inventory.py
+++ 2018/02-inventory/inventory.py
@@ -1,25 +1,18 @@
 #!/usr/bin/env python
 
 
+from collections import Counter
 import fileinput
-
-
-def has_two_or_three(value: str) -> bool:
-    char_counts = {}
-    for char in value:
-        count = char_counts.get(char, 0) + 1
-        char_counts[char] = count
-    return (2 in char_counts.values(), 3 in char_counts.values())
 
 
 def part_one(package_ids: list) -> int:
     match_two = 0
     match_three = 0
     for package_id in package_ids:
-        has_two, has_three = has_two_or_three(package_id)
-        if has_two:
+        counts = [count for _,count in Counter(package_id).most_common()]
+        if 2 in counts:
             match_two += 1
-        if has_three:
+        if 3 in counts:
             match_three += 1
     return match_two * match_three
 
