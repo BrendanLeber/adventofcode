9db304c1c7b9b416d7d33b81135dd427c10b61c3
Brendan Leber <brendan@brendanleber.com>
2018-12-06 21:32:31 -0800

    Advent of Code 2018; Day 04; Part 02.

--- 2018/04-repose/repose.py
+++ 2018/04-repose/repose.py
@@ -5,7 +5,7 @@ from collections import defaultdict
 import fileinput
 
 
-def argmax(d, guard_id):
+def argmax_part_one(d, guard_id):
     best = None
     for k, v in d.items():
         if best is None or (k[0] == guard_id and v > d[best]):
@@ -13,9 +13,17 @@ def argmax(d, guard_id):
     return best
 
 
+def argmax_part_two(d):
+    best = None
+    for k, v in d.items():
+        if best is None or v > d[best]:
+            best = k
+    return best
+
+
 def parse_time(line: str) -> int:
     words = line.split()
-    date, time = words[0][1:], words[1][:-1]
+    time = words[1][:-1]
     return int(time.split(":")[1])
 
 
@@ -38,7 +46,29 @@ def part_one(records: list) -> int:
                 total_asleep[guard] += 1
 
     max_guard = max(total_asleep, key=total_asleep.get)
-    guard, minutes = argmax(minutes_asleep, max_guard)
+    guard, minutes = argmax_part_one(minutes_asleep, max_guard)
+    return guard * minutes
+
+
+def part_two(records: list) -> int:
+    total_asleep = defaultdict(int)
+    minutes_asleep = defaultdict(int)
+    guard = None
+    asleep = None
+
+    for record in records:
+        time = parse_time(record)
+        if "begins shift" in record:
+            guard = int(record.split()[3][1:])
+            asleep = None
+        elif "falls asleep" in record:
+            asleep = time
+        elif "wakes up" in record:
+            for minute in range(asleep, time):
+                minutes_asleep[(guard, minute)] += 1
+                total_asleep[guard] += 1
+
+    guard, minutes = argmax_part_two(minutes_asleep)
     return guard * minutes
 
 
@@ -49,3 +79,4 @@ if __name__ == "__main__":
     records.sort()
 
     print(part_one(records))
+    print(part_two(records))
