e589d1377d71b61833defe74641e32b36fd3ed13
Brendan Leber <brendan@brendanleber.com>
2018-12-12 19:50:15 -0800

    Advent of Code 2018; Day 8; Part 2.

--- 2018/08-memory/memory.py
+++ 2018/08-memory/memory.py
@@ -25,6 +25,17 @@ class Node:
             self.meta_sum += child.sum_metadata()
         return self.meta_sum
 
+    def sum_metaindex(self):
+        self.meta_sum = 0
+        if not self.children:
+            self.meta_sum = sum(self.metadata)
+            return self.meta_sum
+
+        for idx in self.metadata:
+            if 0 < idx <= len(self.children):
+                self.meta_sum += self.children[idx - 1].sum_metaindex()
+        return self.meta_sum
+
 
 def build_tree(values):
     num_children = values.pop(0)
@@ -44,6 +55,10 @@ def part_one(root):
     return root.sum_metadata()
 
 
+def part_two(root):
+    return root.sum_metaindex()
+
+
 if __name__ == "__main__":
     data = []
     for line in fileinput.input():
@@ -52,3 +67,4 @@ if __name__ == "__main__":
     tree = build_tree(data)
 
     print(part_one(tree))
+    print(part_two(tree))
